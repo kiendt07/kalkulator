871555139af1a1fcfec00668d5022c6832db55da
Kien Do <kiendo95@gmail.com>
2021-10-24 01:24:45 +0700

    init create react app

--- kalkulator.ts
+++ kalkulator.ts
@@ -1,14 +1,12 @@
 function hasPrecedence(op1: string, op2: string): boolean {
-  if (op2 == '(' || op2 == ')') {
+  if (op2 === '(' || op2 === ')') {
     return false;
   }
-  if ((op1 == '*' || op1 == '/') &&
-    (op2 == '+' || op2 == '-')) {
+  if ((op1 === '*' || op1 === '/')
+    && (op2 === '+' || op2 === '-')) {
     return false;
   }
-  else {
-    return true;
-  }
+  return true;
 }
 
 function applyOp(op: string, b: number, a: number): number {
@@ -20,21 +18,23 @@ function applyOp(op: string, b: number, a: number): number {
     case '*':
       return a * b;
     case '/':
-      if (b == 0) {
-        document.write("Cannot divide by zero");
+      if (b === 0) {
+        document.write('Cannot divide by zero');
       }
       return a / b;
+    default:
   }
   return 0;
 }
 
 const numberLookAhead = (s: string, i: number): number => {
-  while (s[i+1] >= '0' && s[i+1] <= '9' || s[i+1] === '.') i++;
-  return i;
-}
+  let index: number = i;
+  while ((s[index + 1] >= '0' && s[index + 1] <= '9') || s[index + 1] === '.') index += 1;
+  return index;
+};
 
 const evaluate = (s: string): number => {
-  if (!s) throw 'Cannot evaluate';
+  if (!s) throw new Error('Cannot evaluate');
 
   const ops: string[] = [];
   const values: number[] = [];
@@ -42,7 +42,7 @@ const evaluate = (s: string): number => {
   let i = 0;
   while (i < s.length) {
     const token = s[i];
-    if (token >= '0' && token <= '9' || token === '.') {
+    if ((token >= '0' && token <= '9') || token === '.') {
       const num = s.slice(i, numberLookAhead(s, i) + 1);
       values.push(parseFloat(num));
       i = numberLookAhead(s, i) + 1;
@@ -53,7 +53,7 @@ const evaluate = (s: string): number => {
         values.push(applyOp(ops.pop()!, values.pop()!, values.pop()!));
       }
       ops.push(token);
-      i++;
+      i += 1;
     }
   }
 
@@ -66,18 +66,18 @@ const evaluate = (s: string): number => {
   // Top of 'values' contains
   // result, return it
   return values.pop()!;
-}
+};
 
 const tests = [
   { expression: '3+2', result: 5 },
   { expression: '3+2+4', result: 9 },
   { expression: '3+2*3', result: 9 },
-  { expression: '30+2*3', result: 36},
+  { expression: '30+2*3', result: 36 },
   { expression: '3+6/2', result: 6 },
   { expression: '3+6/2*3', result: 12 },
   { expression: '3+6/2*3-1', result: 11 },
   { expression: '0', result: 0 },
   { expression: '3.2+3', result: 6.2 },
   { expression: '3.2*2+3', result: 9.4 },
-]
-tests.forEach((test, i) => console.log(`${i}. ${test.expression}=${evaluate(test.expression)} ${evaluate(test.expression) === test.result ? '✅' : '❌' + ' (should be ' + test.result + ')'}`));
+];
+tests.forEach((test, i) => console.log(`${i}. ${test.expression}=${evaluate(test.expression)} ${evaluate(test.expression) === test.result ? '✅' : `${'❌ (should be '}${test.result})`}`));
